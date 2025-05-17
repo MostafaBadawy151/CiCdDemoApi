2b9a246a0a7addf733f8c1d117c6c17d1a14d4ea
Mostafa Ahmed <Mostafa.Ahmed@LinkDev.com>
2025-05-17 07:21:31 +0300

    AddProductController

---
+++ CiCdDemoApi/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CiCdDemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(new {Id = 1, Name= "Samsung", Price = 23232});
+        }
+    }
+}
